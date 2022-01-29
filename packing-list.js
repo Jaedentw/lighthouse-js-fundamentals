90a30e08b48a167bc74f3e4e442a5e09236888a1
Jaeden West <jaeden.t.west@gmail.com>
2022-01-28 22:46:29 -0700

    Lighthouse java practice

---
+++ packing-list.js
@@ -0,0 +1,15 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+packingList[3];
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
